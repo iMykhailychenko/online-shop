6ebfc378f0a088f89937ccf86555b680e66daa3e
iMykhailychenko <igor.c.m@ukr.net>
2021-03-01 00:18:29 +0200

    general commit

--- prod.sh
+++ prod.sh
@@ -2,42 +2,24 @@
 G='\033[0;32m' # green
 N='\033[0m' # none
 
-# compile
+# COMPILE PROD
 echo -n "${G}Compile build version:${N}"
 
 echo -n "${G}yarn run build client${N}"
 cd ./client/ && yarn run build && cd ../
 
-
 echo -n "${G}remove old client${N}"
-cd ./server && rm -rf client && cd ../
+cd ./server && rm -rf client
+
+echo -n "${G}yarn run build server${N}"
+yarn run build && cd ../
 
 echo -n "${G}replace client with new files${N}"
 mv ./client/build ./server/client
 
 
-# # INPUT
-# echo -n "${G}Commit message:${N}"
-# read COMMIT
+# PUSH ON GIT
+./git.sh
 
-# # BACKEND
-# echo "${G}\n\nSTART UPLOADING #######${N}"
-
-# # LINT
-# echo "${G}LINTING #######${N}"
-# #yarn run lint
-
-# # GIT
-# echo "${G}\n\nPUSH ON GIT #######${N}"
-# git add .
-
-# if [[ -z "$COMMIT" ]]; then
-#         git commit -m 'general commit'
-# elif [[ -n "$COMMIT" ]]; then
-#         git commit -m "$COMMIT"
-# fi
-
-# git push
-# echo "${G}DONE WITH GIT #######${N}"
 
 # yarn --cwd ./server run start
---
+++ server/client/static/js/auth-page.c8683123.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonponline-shop"]=this["webpackJsonponline-shop"]||[]).push([[0],{60:function(n,s,t){"use strict";t.r(s);t(0);var e=t(1);s.default=function(){return Object(e.jsx)("div",{children:"Auth"})}}}]);
+//# sourceMappingURL=auth-page.c8683123.chunk.js.map
