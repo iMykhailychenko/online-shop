4ca5b862f9f8e7e7cb0fc8fd2475ba5415ce6a3d
iMykhailychenko <igor.c.m@ukr.net>
2021-03-05 01:25:58 +0200

    pull

---
+++ server/dist/src/pictures/pictures.pipe.d.ts
@@ -0,0 +1,4 @@
+import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+export declare class PicturesPipe implements PipeTransform {
+    transform(value: any, metadata: ArgumentMetadata): any;
+}
