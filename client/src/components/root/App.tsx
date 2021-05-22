33adbf9818c80bfaa26aec12a79dd12d4f887da4
iMykhailychenko <igor.c.m@ukr.net>
2021-05-22 21:01:52 +0300

    general commit

--- client/src/components/root/App.tsx
+++ client/src/components/root/App.tsx
@@ -1,10 +1,10 @@
-import React, { ReactElement, Suspense } from 'react';
+import React, { ReactElement } from 'react';
 import ReactNotification from 'react-notifications-component';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 
 import router from '../../assets/router';
 import CartDrawer from '../common/Drawer/CartDrawer';
-import ModalComponent from '../common/Modal';
+import ModalComponent, { modal } from '../common/Modal';
 import ScrollCart from '../common/ScrollCart';
 import ScrollTop from '../common/ScrollTopBtn';
 import Body from '../layout/Body';
@@ -13,35 +13,30 @@ import Header from '../layout/Header';
 import css from './index.module.css';
 
 const App = (): ReactElement => {
-    return (
-        <Suspense fallback={null}>
-            <BrowserRouter>
-                <div className={css.main}>
-                    <Header />
-                    <Body>
-                        <Switch>
-                            <Route exact path={router.home.path} component={router.home.component} />
-                            <Route path={router.auth.path} component={router.auth.component} />
-                            <Route
-                                exact
-                                path={router.product.create.path}
-                                component={router.product.create.component}
-                            />
-                            <Route path={router.product.single.path} component={router.product.single.component} />
-                            <Route path={router.profile.path} component={router.profile.component} />
-                        </Switch>
-                    </Body>
-                    <CartDrawer />
-                    <ScrollCart />
-                    <ScrollTop />
-                    <Footer />
+    const history = useHistory();
+    history?.listen(modal.close);
 
-                    {/* Utils */}
-                    <ModalComponent />
-                    <ReactNotification />
-                </div>
-            </BrowserRouter>
-        </Suspense>
+    return (
+        <div className={css.main}>
+            <Header />
+            <Body>
+                <Switch>
+                    <Route exact path={router.home.path} component={router.home.component} />
+                    <Route path={router.auth.path} component={router.auth.component} />
+                    <Route exact path={router.product.create.path} component={router.product.create.component} />
+                    <Route path={router.product.single.path} component={router.product.single.component} />
+                    <Route path={router.profile.path} component={router.profile.component} />
+                </Switch>
+            </Body>
+            <CartDrawer />
+            <ScrollCart />
+            <ScrollTop />
+            <Footer />
+
+            {/* Utils */}
+            <ModalComponent />
+            <ReactNotification />
+        </div>
     );
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,20 @@
 import 'react-notifications-component/dist/theme.css';
 import './styles/styles.css';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Suspense fallback={null}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Suspense>
     </React.StrictMode>,
     document.getElementById('root'),
 );
